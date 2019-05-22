e8bfc0e7cd8949bcd9ef5ea11646d102cfc6fa3c
Mourizio Zeland <zelandmourizio@gmail.com>
2019-05-21 21:24:44 -0700

    newnew

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,12 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+  host: "newhost",
   port: 3306,
   user: "root",
   password: "password",
@@ -17,6 +21,7 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-
+}
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
